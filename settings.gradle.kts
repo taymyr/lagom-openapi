dcfd0d9e6c755d6744e7afeef5a10d91aff0b5b4
removedporn <86824510+removedporn@users.noreply.github.com>
2021-11-19 14:51:38 +0800

    Delete settings.gradle.kts

--- settings.gradle.kts
+++
@@ -1,8 +0,0 @@
-include("core", "java:api", "java:impl", "scala:api", "scala:impl")
-
-rootProject.name = "lagom-openapi"
-project(":core").name = "lagom-openapi-core"
-project(":java:api").name = "lagom-openapi-java-api"
-project(":java:impl").name = "lagom-openapi-java-impl"
-project(":scala:api").name = "lagom-openapi-scala-api"
-project(":scala:impl").name = "lagom-openapi-scala-impl"
