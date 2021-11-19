79930dcf9acd409c6cbed3c7aefc6cb94aaee24d
removedporn <86824510+removedporn@users.noreply.github.com>
2021-11-19 14:42:21 +0800

    Delete java directory

--- java/impl/src/test/kotlin/org/taymyr/lagom/javadsl/openapi/file/Test4ServiceImpl.kt
+++
@@ -1,6 +0,0 @@
-package org.taymyr.lagom.javadsl.openapi.file
-
-import com.typesafe.config.Config
-import org.taymyr.lagom.javadsl.openapi.AbstractOpenAPIService
-
-class Test4ServiceImpl(config: Config) : AbstractOpenAPIService(config), Test4Service
